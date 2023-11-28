9138ef2be8990af6095d9943e99d8699849e2ef2
Barbs02 <114110721+Barbs02@users.noreply.github.com>
2023-11-28 13:44:07 -0300

    s

--- prueba.php
+++ prueba.php
@@ -7,7 +7,7 @@ Template Name: Prueba
 <?php get_header(); ?>
 
 <script>
-fetch("https://basubero.laboratoriodiseno2.cl/barb_playlist/wp-json")
+fetch("https://basubero.laboratoriodiseno2.cl/barb_playlist/wp-json	/wp/v2/posts")
 .then(response => response.ok ? response.json() :Promise.reject(response))
 .then(data => {
     console.log(data);
