46d6575822f65c269dedc2ba37dd8355a3ed85f8
Barbs02 <114110721+Barbs02@users.noreply.github.com>
2023-11-28 13:03:27 -0300

    j

--- js/script.js
+++ js/script.js
@@ -1,9 +1,46 @@
 // Aparecer primera alerta----------------------------------------------------------
 
-function alerta(){
-    let alert = document.getElementById('alerta');
-    alert.style.display= 'block';
-} 
+
+fetch("https://seortizm.laboratoriodiseno2.cl/spotiify/wp-json/wp/v2/posts?_fields=id,acf&acf_format=standard&categories=21")
+.then(response => response.json())
+.then(data => {
+    caparazon(data);
+    console.log(data);
+})
+
+let tarjetas = document.getElementById('rowsito'); // Seleccionar el contenedor
+
+function caparazon(data) {
+    data.forEach(posts => {
+
+        const tarjeta = document.createElement('div')
+        tarjeta.classList.add('col-md-3', 'canciondatos');
+
+        let imagenCarta = document.createElement('img');
+        imagenCarta.classList.add('img-fluid','mb-5','w-100','mt-5')
+        imagenCarta.src = posts.acf.imagen_tarjeta.url;
+
+        const tituloCarta = document.createElement('h5');
+        tituloCarta.classList.add('titulodelacarta','mb-5','text-white')
+        tituloCarta.innerHTML = posts.acf.nombre_tarjeta;
+
+        const artistaCarta = document.createElement('p');
+        artistaCarta.classList.add('mb-5','text-white')
+        artistaCarta.innerHTML = posts.acf.descripcion_tarjeta;
+
+        const mp3 = document.createElement('audio');
+        mp3.classList.add('col-md-8')
+        mp3.controls = true;
+        mp3.src = posts.acf.cancion.url;
+
+        tarjeta.appendChild(imagenCarta);
+        tarjeta.appendChild(tituloCarta);
+        tarjeta.appendChild(artistaCarta);
+        tarjeta.appendChild(mp3);
+
+        tarjetas.appendChild(tarjeta); // Agregar la tarjeta al contenedor
+    })
+}
 
 //Script de swiper----------------------------------------------------------------
 var swiper = new Swiper(".mySwiper", {
